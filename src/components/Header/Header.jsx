c6689afe601dc78d1dbc899eba2d863ee7fffc8e
Shaik Sameer <130442189+Shaik-Sameer77@users.noreply.github.com>
2024-02-13 23:50:21 +0530

    done for the day

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+const selectStyle = {
+    background: `url('https://waf.barracuda.com/cui/images/formalize/select_arrow.gif') no-repeat right center`,
+    WebkitAppearance: 'none',
+    MozAppearance: 'none',
+    appearance: 'none',
+    paddingRight: '20px', // Adjust the padding to make space for the arrow image
+  };
 
 const Header = () => {
+    
   return (
-    <div>Header</div>
-  )
-}
+    <header style={{display:"flex", justifyContent:"flex-end", backgroundColor:"#223355", height:"30px",zIndex:"100"}}>
+      <nav style={{backgroundColor:"#1a2640",padding:"3px 10px 5px 10px"}}>
+        <select id="languageSelect" name="language"  style={{borderRadius:"3px", padding:"2px 40px 2px 0px", font:"11px Arial",marginRight:"5px",marginLeft:"4px", outline:"none" }}>
+          <option value="english">English</option>
+          <option value="spanish">Spanish</option>
+          <option value="french">French</option>
+        </select>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const styles = {
+  height: '100vh',
+  width: '100vw',
+  background: 'linear-gradient(to top, #001133, #3b5885)',
+};
+
+const Hero = () => {
+  return <div style={styles}>Hero</div>;
+};
+
+export default Hero;
